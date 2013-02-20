85a53bd34420e17e85799b37c02e7d7daec27aff
Víctor Dueñas <victor.duenas@babel.es>
2013-02-20 21:06:16 +0100

    Esta en funcionamiento el menu básico.
    Ahora mismo el único botón que tiene un evento es el primero que abre la
    primera actividad. Esto lo he hecho de ejemplo para que lo podais
    reproducir para las otras 2 actividades que hay que hacer.

--- gastos/src/com/es/babelstore/ejemplos/gastos/MainActivity.java
+++ gastos/src/com/es/babelstore/ejemplos/gastos/MainActivity.java
@@ -1,15 +1,29 @@
 package com.es.babelstore.ejemplos.gastos;
 
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+	Button btnIngreGas;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		btnIngreGas = (Button)findViewById(R.id.btnIngreGas);
+		btnIngreGas.setOnClickListener(new OnClickListener() {
+        	public void onClick(View v) {
+        		Intent intent = new Intent();
+        		// Leemos lo que se ha introducido en la caja de texto
+        		intent.setClass(getApplicationContext(), IngreGasActivity.class);
+        		startActivity(intent);
+        	}
+        });
 	}
 
 	@Override
